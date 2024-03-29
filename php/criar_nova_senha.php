092c139410b10cff2e51b1d3f16b26ad7ab3fe9b
Elisa de Oliveira Martins <elisa98martins@gmail.com>
2024-03-29 18:22:52 -0300

    funcionou confirmacao e redefinicao

---
+++ php/criar_nova_senha.php
@@ -0,0 +1,63 @@
+<?php
+
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$con = mysqli_connect("localhost", "root", "", "buscavet");
+if (!$con) {
+    die("Falha na conexão: " . mysqli_connect_error());
+}
+
+function validarSenha($senha) {
+    $regex = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/';
+    return preg_match($regex, $senha);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $token = $_POST['token'];
+    $new_password = $_POST['new_password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    if ($new_password !== $confirm_password) {
+        echo json_encode(["mensagem" => "As senhas não coincidem."]);
+        exit;
+    }
+
+    if (!validarSenha($new_password)) {
+        echo json_encode(["mensagem" => "A senha não atende aos requisitos de segurança."]);
+        exit;
+    }
+
+    $stmt = $con->prepare("SELECT * FROM usuario WHERE token = ? AND token_expira > NOW()");
+    $stmt->bind_param("s", $token);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+
+    if ($resultado->num_rows == 1) {
+        $usuario = $resultado->fetch_assoc();
+        $passwordHashed = password_hash($new_password, PASSWORD_DEFAULT);
+
+        $updateStmt = $con->prepare("UPDATE usuario SET password = ?, token = '', token_expira = NULL WHERE id = ?");
+        $updateStmt->bind_param("si", $passwordHashed, $usuario['id']);
+        $updateStmt->execute();
+
+        if ($updateStmt->affected_rows == 1) {
+            echo json_encode(["mensagem" => "Senha atualizada com sucesso."]);
+        } else {
+            echo json_encode(["mensagem" => "Erro ao atualizar a senha."]);
+        }
+        $updateStmt->close();
+    } else {
+        echo json_encode(["mensagem" => "Token inválido ou expirado."]);
+    }
+
+    $stmt->close();
+}
+
+mysqli_close($con);
+
+?>
--- php/redefinicao.php
+++ php/redefinicao.php
@@ -1,37 +1,44 @@
+<!-- redefinicao.php -->
 <?php
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'PHPMeiler-master/src/Exeception.php';
-require 'PHPMeiler-master/src/PHPMeiler.php';
-require 'PHPMeiler-master/src/SMTP.php';
-$codigo = rand(100,999);
-$mail = new PHPMailer();
-
-//Config
-$mail->Mailer = "smtp";
-$mail->IsSMTP();
-$mail->CharSet = 'UTF-8';
-$mail->SMTPDebug = 1;
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'ssl';
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 465;
-
-//Detalhes envio do E-mail
-
-$mail->Username = 'buscavet';
-$mail->Password = 'senhaforte';
-$mail->SetFrom('buscavet@gmail.com', 'BUSCAVET');
-
-$mail->addAddress('destinatario@gmail.com', '');
-$mail->Subject = "Assunto do E-mail";
-
-$mail->msgHTML = $codigo;
-
-if(!$mail->Send()) { 
-    echo "Mailer Error: " . $mail->ErrorInfo; 
- } else { 
-    echo "Mensagem enviada com sucesso"; 
- } 
+$token = $_GET['token'] ?? '';
 ?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/style.css">
+    <title> BuscaVet - Redefinição de Senha </title>
+    <link rel="icon" href="/img/Logo.png">
+</head>
+<body style="background-image: url('../img/BuscaVetLogo.png');">
+
+<header class="cabecalho">
+    <div class="juntos">
+    <div class="logo">
+        <a href="/img/imagem.png">
+            <img src="../img/Logo.png" width="120" height="120">
+        </a>
+
+        <ul>
+            <li><a href="index.html" class="link">HOME</a></li>
+            <li><a href="login.html" class="link">LOGIN</a></li>
+            <li><a href="cadastro_usuario.html" class="link">CADASTRE-SE</a></li>
+            <li><a href="autenticacao.html" class="link">AUTENTICACAO</a></li>
+        </ul>
+</header>
+    <h1 id="titulo_geral">Redefinição de Senha</h1>
+    <div id="nova-senha" class="nova-senha">
+        <form id="form-nova-senha" action="criar_nova_senha.php" method="post" class="form-nova-senha">
+            <input type="hidden" name="token" value="<?php echo htmlspecialchars($token); ?>">
+            <div class="input-container">
+                <label for="new_password">Nova Senha:</label>
+                <input type="password" name="new_password" id="new_password" required>
+            </div>
+            <div class="input-container">
+                <label for="confirm_password">Confirmar Nova Senha:</label>
+                <input type="password" name="confirm_password" id="confirm_password" required>
+            </div>
+            <button type="submit">Redefinir Senha</button>
+        </form>
+    </div>
+</body>
+</html>
